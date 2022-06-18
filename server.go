d39d464b67a1697d6103a374c18bcda527e12c5e
retorica-puzzle <gungkego@outlook.com>
2022-06-18 22:33:15 +0800

    testing and change name folder compiled

--- server.go
+++ server.go
@@ -7,7 +7,6 @@ import (
 )
 
 func main() {
-
 	http.Handle("/", http.FileServer(http.Dir("./views")))
 	fmt.Println("Server starting at http://localhost:3000/")
 	log.Fatal(http.ListenAndServe(":3000", nil))
